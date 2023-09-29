c18800a3d0750e4162f08d47afcf9a0417d6eabd
skimani-91 <119099854+skimani-91@users.noreply.github.com>
2023-09-29 13:45:07 +0300

    Add files via upload

---
+++ simpcalc.py
@@ -0,0 +1,7 @@
+# Kimani Owen Mwangi
+
+num1 = int(input("Enter your first number "))
+num2 = int(input("Enter your second number "))
+
+summ = num1 + num2
+print("The sum of the two numbers will be",summ)
